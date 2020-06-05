3364d6d3c8bcfaefde12a00833db6d4906170873
Sho Taguchi <taguchi.sho@mail.canon>
2020-06-05 12:56:59 +0900

    AGC033A pypy3でクリア

--- AGC033A/resolve.py
+++ AGC033A/resolve.py
@@ -1,49 +1,44 @@
 def resolve():
     '''
     code here
+    pypy3で提出すればOK
+    pythonはTLE
     '''
     import collections
     H, W = [int(item) for item in input().split()]
     grid = [[item for item in input()] for _ in range(H)]
+
+    fp = [[-1 for _ in range(W)] for _ in range(H)]
+
     que = collections.deque()
     next_y_x = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    
 
-    res = 0
-
-    is_not_all_black = False
     for i in range(H):
         for j in range(W):
-            if grid[i][j] == '.':
-                is_not_all_black = True
-                break
-        else:
-            if is_not_all_black:
-                break
+            if grid[i][j] == '#':
+                que.append([i, j, 0])
+                fp[i][j] = 0
 
-    
-    while is_not_all_black:
-        res += 1
-        is_not_all_black = False
-        for i in range(H):
-            for j in range(W):
-                if grid[i][j] == '#':
-                    que.append([i, j])
-                else:
-                    is_not_all_black = True
+    while que:
+        temp = que.popleft()
 
-        while que:
-            temp = que.popleft()
+        for dy, dx in next_y_x:
+            ny = temp[0] + dy
+            nx = temp[1] + dx
 
-            for dy, dx in next_y_x:
-                ny = temp[0] + dy
-                nx = temp[1] + dx
+            if 0 <= ny <= H-1 and 0 <= nx <= W-1:
+                if fp[ny][nx] == -1:
+                    que.append([ny, nx, temp[2]+1])
+                    fp[ny][nx] = temp[2] + 1
 
-                if 0 <= ny <= H-1 and 0 <= nx <= W-1:
-                    if grid[ny][nx] == '.':
-                        grid[ny][nx] = '#'
+    temp_max_list = []
+    for line in fp:
+        temp_max_list.append(max(line))
+
+    print(max(temp_max_list))
 
 
-    print(res-1)
 
 if __name__ == "__main__":
     resolve()
